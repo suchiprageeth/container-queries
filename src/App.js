f27e2903bc5caca5b039001602515165d9fbe838
suchiprageeth <suchiruprageeth94@gmail.com>
2022-12-18 08:21:48 +0530

    adding polyfil package to support container queries for firefox

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from "./logo.svg";
 import "./App.css";
+import movies from "./movies.json";
+
+const Card = ({ image, title, subtitle, description }) => {
+  return (
+    <div className="card">
+      <div
+        className="image"
+        style={{ backgroundImage: `url(/${image})` }}
+      ></div>
+
+      <div className="info">
+        <h2>{title}</h2>
+        <h4>{subtitle}</h4>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+};
 
 function App() {
+  // console.log(16, movies[0]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="layout">
+        <main className="container">
+          <Card {...movies[0]} />
+          <Card {...movies[1]} />
+          <Card {...movies[2]} />
+        </main>
+        <article className="container">
+          <Card {...movies[3]} />
+          <Card {...movies[2]} />
+          <Card {...movies[1]} />
+        </article>
+      </div>
     </div>
   );
 }
